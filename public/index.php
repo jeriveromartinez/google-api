5e031544b9edda237320831b3d7ed281e6834f82
Julio Rivero <jeriveromartinez@gmail.com>
2017-05-15 11:46:35 -0400

    try

--- public/index.php
+++ public/index.php
@@ -14,6 +14,14 @@ define('SCOPES', implode(' ', array(
  */
 function getClient()
 {
+    session_start();
+
+    if ((isset($_SESSION)) && (!empty($_SESSION))) {
+        echo "There are cookies<br>";
+        echo "<pre>";
+        print_r($_SESSION);
+        echo "</pre>";
+    }
     $client = new Google_Client();
     $client->setApplicationName(APPLICATION_NAME);
     $client->setScopes(SCOPES);
@@ -21,20 +29,38 @@ function getClient()
     $client->setIncludeGrantedScopes(true);
     $client->setAccessType('offline');
 
-    // Load previously authorized credentials from a file.
-    if (file_exists(CREDENTIALS_PATH)) {
-        $accessToken = file_get_contents(CREDENTIALS_PATH);
-    } else {
-        // Request authorization from the user.
-        $authUrl = $client->createAuthUrl();
-        header("Location: $authUrl");
+    if (isset($_GET['code'])) {
+        echo "<br>I got a code from Google = " . $_GET['code']; // You won't see this if redirected later
+        $client->authenticate($_GET['code']);
+        $_SESSION['token'] = $client->getAccessToken();
+        header('Location: http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF']);
+        echo "<br>I got the token = " . $_SESSION['token']; // <-- not needed to get here unless location uncommented
     }
-    $client->setAccessToken($accessToken);
 
-    // Refresh the token if it's expired.
-    if ($client->isAccessTokenExpired()) {
-        $client->refreshToken($client->getRefreshToken());
-        file_put_contents(CREDENTIALS_PATH, $client->getAccessToken());
+    if (isset($_SESSION['token'])) {
+        echo "<br>Getting access";
+        $client->setAccessToken($_SESSION['token']);
+    }
+
+    if ($client->getAccessToken()) {
+        echo "<hr><font size=+1>I have access to your calendar</font>";
+        $event = new Google_Event();
+        $event->setSummary('Halloween');
+        $event->setLocation('The Neighbourhood');
+        $start = new Google_EventDateTime();
+        $start->setDateTime('2013-9-29T10:00:00.000-05:00');
+        $event->setStart($start);
+        $end = new Google_EventDateTime();
+        $end->setDateTime('2013-9-29T10:25:00.000-05:00');
+        $event->setEnd($end);
+        $createdEvent = $cal->events->insert('###', $event);
+        echo '<br><font size=+1>Event created</font>';
+
+        echo '<hr><br><font size=+1>Already connected</font> (No need to login)';
+
+    } else {
+        $authUrl = $client->createAuthUrl();
+        print "<hr><br><font size=+2><a href='$authUrl'>Connect Me!</a></font>";
     }
     return $client;
 }
